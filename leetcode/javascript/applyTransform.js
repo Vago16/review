afc6f4f2aa8c2bca0bc1000a53c37a14a555d92e
Vago16 <vagelispetropoulos@yahoo.com>
2024-08-02 17:07:50 -0400

    created applyTransform.js

---
+++ leetcode/javascript/applyTransform.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        res.push(fn(arr[i], Number(i)));
+    }
+    return res;
+};
