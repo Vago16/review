a27a51e89187fc0017a247979e160a999d8461ab
Vago16 <vagelispetropoulos@yahoo.com>
2024-08-15 15:02:28 -0400

    set up constants in js file

---
+++ javascript/whackAMole/script.js
@@ -0,0 +1,7 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const button = document.querySelector('#start');
+let lastHole;
+let timeUp = false;
+let score = 0;
