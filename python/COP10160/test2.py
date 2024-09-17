4386075e31908eca015c25642ad53962a4763864
Vago16 <vagelispetropoulos@yahoo.com>
2024-09-17 18:05:04 -0400

    update test2.py

--- python/COP10160/test2.py
+++ python/COP10160/test2.py
@@ -1,2 +1,6 @@
+'''import sys imports the sys modules that is built into Python into the current module.
+   This module provides access to Python interpreter functions.
+    An argument is a value passed to a function it is called.
+     sys.argv[] lets us pass in an argument after we run the script.'''
 import sys
 print("This argument was passed to the script:", sys.argv[1], sys.argv[2], sys.argv[3])
