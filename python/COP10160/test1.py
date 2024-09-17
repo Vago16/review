0be84a6cb958e0c5cb0de3b91dd2c5f1577a0e54
Vago16 <vagelispetropoulos@yahoo.com>
2024-09-17 17:53:52 -0400

    create COP10160 folder and test1.py

---
+++ python/COP10160/test1.py
@@ -0,0 +1,4 @@
+''' Purposely print a syntax error, take out the hash symbol on line 3 to see '''
+print("---------------------------------")
+#print(invalid instruction) 
+print("---------------------------------")
