0ad9017ee7ca3239d7f22e12c62692304d914d51
Vago16 <vagelispetropoulos@yahoo.com>
2024-09-17 18:00:02 -0400

    added notes to test1.py

--- python/COP10160/test1.py
+++ python/COP10160/test1.py
@@ -1,4 +1,5 @@
-''' Purposely print a syntax error, take out the hash symbol on line 3 to see '''
+''' Purposely print a syntax error if using (invalid instructio) or other things, take out the hash symbol on line 4 to see, otherwise prints Hello 100 times '''
 print("---------------------------------")
+print("Hello " * 100)
 #print(invalid instruction) 
 print("---------------------------------")
