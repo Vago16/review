7fefdb076c75b35dc3bb63f99125872f0483dcde
Vago16 <vagelispetropoulos@yahoo.com>
2024-09-22 00:28:21 -0400

    uploaded get_first_n_elements.py

---
+++ python/COP10160/Chapter2/get_first_n_elements.py
@@ -0,0 +1,7 @@
+array = [1, 2 , 3, 4, 5, 6, 7, 8, 9]
+
+print("Array: ", array)
+
+n = int(input("Number of elements to fetch from array: "))
+#print out slice of array from first element to nth element
+print(array[:n])
