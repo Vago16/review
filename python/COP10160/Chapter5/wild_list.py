f74e94bba7cded69c9ef46a4f5d52c96c6b24e07
Vago16 <vagelispetropoulos@yahoo.com>
2024-10-31 15:07:58 -0400

    add new exercises

---
+++ python/COP10160/Chapter5/wild_list.py
@@ -0,0 +1,18 @@
+wild = ["Lion", "Zebra", "Panther", "Antelope"]
+animals = []
+
+
+wild.append("Elephant")
+
+animals.extend(wild)
+
+
+animals.insert(2,"Cheetah")
+
+
+animals.pop(1)
+animals.insert(1,"Giraffe")
+
+animals.sort(key=None, reverse=False)
+
+
