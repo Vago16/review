c746bc37eab57a997bbb0868b23283212b9eaeb8
Vago16 <vagelispetropoulos@yahoo.com>
2024-10-31 16:15:40 -0400

    added exercise

---
+++ python/COP10160/Chapter5/tuple_methods.py
@@ -0,0 +1,16 @@
+animals = 'cat', 'dog', 'horse'
+
+#any() method
+print(any(animals))
+
+#count() method
+print(animals.count("cat"))
+
+#min() method
+print (min(animals))
+
+#max() method
+print (max(animals))
+
+#len() method
+print (len(animals))
