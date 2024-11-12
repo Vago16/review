83d7abc1f84eae6bedc30a310593825a8884ce53
Vago16 <vagelispetropoulos@yahoo.com>
2024-11-12 14:20:32 -0500

    added multiple exercises

---
+++ python/COP10160/Chapter7/inheritance.py
@@ -0,0 +1,14 @@
+class Person(object):
+    def __init__(self, name):
+         self.name = name
+    def getName(self):
+         return self.name
+    def isEmployee(self):
+         return False
+class Employee(Person):
+    def isEmployee(self):
+         return True
+emp = Person("ABC")
+print(emp.getName(), emp.isEmployee())
+emp = Employee("XYZ")
+print(emp.getName(), emp.isEmployee())
