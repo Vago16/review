c62d7a2d4a41d40bb8655d61f797e9d6a6e93471
Vago16 <vagelispetropoulos@yahoo.com>
2024-11-12 20:30:20 -0500

    added new exercises

---
+++ python/COP10160/Chapter8/rand_num_gen.py
@@ -0,0 +1,12 @@
+import random
+
+def random_number_generator(l):
+    """
+    Generate a list of random numbers of length l.
+    """
+    output = []
+  
+    for i in range(l):
+        output.append(random.randint(0, 1000))
+    
+    return output
